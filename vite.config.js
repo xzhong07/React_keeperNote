9de791206e9e2b0f64a29c415ee090ce0f44c625
鐘星語 <159898780+uts2-XingyuZhong@users.noreply.github.com>
2026-05-18 11:36:41 -0400

    add build setting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: process.env.GITHUB_PAGES  // この行を追加
+        ? "React_keeperNote"            // この行を追加
+        : "./",                     // この行を追加
+    plugins: [react()],
+});
